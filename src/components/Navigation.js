458595935e66f1eeacd1df736ab2b9589450e164
jianing <lijn2003@protonmail.com>
2024-06-23 17:05:37 +1200

    Custom nav menu icon

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,17 +1,38 @@
+import { useEffect, useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import TypedText from './TypedText';
 import ThemeButton from './ThemeButton';
+import './Navigation.css'
+
+function getMenuIcon(isDark, isExpanded) {
+  if (isDark) {
+    return (
+      isExpanded ?
+      "/images/navigation/cross-yellow.png" :
+      "/images/navigation/menu-yellow.png"
+    );
+  } else {
+    return (
+      isExpanded ?
+      "/images/navigation/cross.png" :
+      "/images/navigation/menu.png"
+    );
+  }
+}
 
 function Navigation({ brand, isDark, onToggleTheme }) {
+  const [isExpanded, setExpanded] = useState(false);
+
   return (
     <Navbar
       expand="lg"
       bg={isDark ? "dark" : "light"}
       data-bs-theme={isDark ? "dark" : "light"}
       fixed="top"
-      >
+      onToggle={() => setExpanded(!isExpanded)}
+    >
       <Container>
         <Navbar.Brand href="#home">
           <img
@@ -24,7 +45,14 @@ function Navigation({ brand, isDark, onToggleTheme }) {
         <Navbar.Brand href="#home">
           <TypedText strings={brand}/>
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle aria-controls="basic-navbar-nav" id="nav-toggle">
+          <img
+            src={getMenuIcon(isDark, isExpanded)}
+            width="32"
+            height="32"
+            alt=""
+          />
+        </Navbar.Toggle>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
             <Nav.Link href="#home">Home</Nav.Link>
