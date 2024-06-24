1ab68f3072a7f8dc6af054f4f60d62c2ad3297c2
jianing <lijn2003@protonmail.com>
2024-06-25 07:56:08 +1200

    Resize slider images

--- src/components/SkillsSlider.js
+++ src/components/SkillsSlider.js
@@ -32,14 +32,14 @@ function SkillsSlider() {
             <div className="icon">
               <img
                 src="/images/skills-slider/html5.svg"
-                width="72"
+                width="64"
                 alt="HTML5"
               />
             </div>
             <div className="icon">
               <img
                 src="/images/skills-slider/css3.svg"
-                width="72"
+                width="64"
                 alt="CSS3"
               />
             </div>
