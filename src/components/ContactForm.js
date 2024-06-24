4e10e95fd82fc300272c50f63bdfa617094eabdf
jianing <lijn2003@protonmail.com>
2024-06-24 15:52:19 +1200

    Contact form validation

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,33 +1,64 @@
+import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import TypedText from './TypedText';
 import './ContactForm.css';
 
 function ContactForm({ isDark }) {
+  const [validated, setValidated] = useState(false);
+
+  function handleSubmit(e) {
+    const form = e.currentTarget;
+    if (form.checkValidity()) {
+      
+    } else {
+      e.preventDefault();
+      e.stopPropagation();
+      setValidated(true);
+    }
+  }
+
   return (
-    <Form className="form">
+    <Form
+      noValidate
+      validated={validated}
+      onSubmit={handleSubmit}
+      className="form"
+    >
       <h2><TypedText strings={["Contact"]} /></h2>
       <Form.Group className="form-group">
         <Form.Control
+          required
           type="text"
           placeholder="Name"
           className="form-control"
         />
+        <Form.Control.Feedback type="invalid">
+          <p>Please enter your name.</p>
+        </Form.Control.Feedback>
       </Form.Group>
       <Form.Group className="form-group">
         <Form.Control
+          required
           type="email"
           placeholder="Email address"
           className="form-control"
         />
+        <Form.Control.Feedback type="invalid">
+          <p>Please enter a valid email address.</p>
+        </Form.Control.Feedback>
       </Form.Group>
       <Form.Group className="form-group">
         <Form.Control
+          required
           as="textarea"
           placeholder="Message"
           rows={8}
           className="form-control"
         />
+        <Form.Control.Feedback type="invalid">
+          <p>Please enter your message.</p>
+        </Form.Control.Feedback>
       </Form.Group>
       <Button
         variant={
