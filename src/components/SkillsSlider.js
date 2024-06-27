b2cdb93cb14f3afeadc8464d017ec326b564bc0a
jianing <lijn2003@protonmail.com>
2024-06-27 17:50:56 +1200

    Update skills slider

--- src/components/SkillsSlider.js
+++ src/components/SkillsSlider.js
@@ -23,7 +23,45 @@ function SkillsSlider() {
           </div>
           <p>
             Most proficient programming language (and also favourite)
-            - the syntax and readabilty cannot be beaten.
+            - the syntax and readability cannot be beaten.
+          </p>
+        </div>
+        <div className="slide">
+          <h3>Kotlin + Android</h3>
+          <div className="icons">
+            <div className="icon">
+              <img
+                src="/images/skills-slider/kotlin.png"
+                width="96"
+                alt="Kotlin"
+              />
+            </div>
+            <div className="icon">
+              <img
+                src="/images/skills-slider/android.svg"
+                width="96"
+                alt="Android"
+              />
+            </div>
+          </div>
+          <p>
+            Experienced with Kotlin syntax and programming concepts.<br />
+            Mobile development on Android.
+          </p>
+        </div>
+        <div className="slide">
+          <h3>Java</h3>
+          <div className="icons">
+            <div className="icon">
+              <img
+                src="/images/skills-slider/java.png"
+                width="128"
+                alt="Java"
+              />
+            </div>
+          </div>
+          <p>
+            Experienced with Java syntax and programming concepts.
           </p>
         </div>
         <div className="slide">
@@ -52,47 +90,40 @@ function SkillsSlider() {
             </div>
           </div>
           <p>
-            Experienced with front-end web development
-            with HTML + CSS + JS.<br />
+            Front-end web development using HTML + CSS + JavaScript.<br />
             Created many webpages such as this one.
           </p>
         </div>
         <div className="slide">
-          <h3>Java</h3>
+          <h3>React.js</h3>
           <div className="icons">
             <div className="icon">
               <img
-                src="/images/skills-slider/java.png"
-                width="128"
-                alt="Java"
+                src="/images/skills-slider/react.svg"
+                width="96"
+                alt="React.js"
               />
             </div>
           </div>
           <p>
-            Experienced with Java syntax and programming concepts.
+            Front-end web development using the React JavaScript library.<br />
+            Created many webpages such as this one.
           </p>
         </div>
         <div className="slide">
-          <h3>Kotlin + Android</h3>
+          <h3>Flask<br />(Python library)</h3>
           <div className="icons">
             <div className="icon">
               <img
-                src="/images/skills-slider/kotlin.png"
-                width="96"
-                alt="Kotlin"
-              />
-            </div>
-            <div className="icon">
-              <img
-                src="/images/skills-slider/android.svg"
-                width="96"
-                alt="Android"
+                src="/images/skills-slider/flask.png"
+                width="72"
+                alt="Flask"
               />
             </div>
           </div>
           <p>
-            Experienced with Kotlin syntax and programming concepts.<br />
-            Mobile development on Android.
+            Back-end web development using the Flask framework.<br />
+            Familiar with building RESTful web APIs.
           </p>
         </div>
       </Slider>
