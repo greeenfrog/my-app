6285cb7e76baf9dac7b413d97983dc4ef2975af6
jianing <lijn2003@protonmail.com>
2024-09-18 16:02:07 +1200

    Replace theme button with MUI switch

--- src/components/ThemeButton.js
+++ src/components/ThemeButton.js
@@ -1,41 +1,56 @@
-import ButtonGroup from 'react-bootstrap/ButtonGroup';
-import ToggleButton from 'react-bootstrap/ToggleButton';
-import './ThemeButton.css'
+// import ButtonGroup from 'react-bootstrap/ButtonGroup';
+// import ToggleButton from 'react-bootstrap/ToggleButton';
+import { Switch } from '@mui/material';
 
 function ThemeButton({ isDark, onToggleTheme }) {
   return (
-    <div id="theme-container">
-      <ButtonGroup>
-        <ToggleButton
-          id="toggle-check"
-          type="checkbox"
-          variant='secondary'
-          checked={isDark}
-          value={1}
-          onChange={() => onToggleTheme()}
-          className="theme-btn"
-          title={
-            isDark ?
-            "Light mode" :
-            "Dark mode"
-          }
-        >
-          <img
-            src={
-              isDark ?
-              "/images/navigation/light-mode.png" :
-              "/images/navigation/dark-mode.png"
-            }
-            width="30"
-            height="30"
-            alt={
-              isDark ?
-              "Light mode" :
-              "Dark mode"
-            }
-          />
-        </ToggleButton>
-      </ButtonGroup>
+    // <div id="theme-container">
+    //   <ButtonGroup>
+    //     <ToggleButton
+    //       id="toggle-check"
+    //       type="checkbox"
+    //       variant='secondary'
+    //       checked={isDark}
+    //       value={1}
+    //       onChange={() => onToggleTheme()}
+    //       className="theme-btn"
+    //       title={
+    //         isDark ?
+    //         "Light theme" :
+    //         "Dark theme"
+    //       }
+    //     >
+    //       <img
+    //         src={
+    //           isDark ?
+    //           "/images/navigation/light-theme.png" :
+    //           "/images/navigation/dark-theme.png"
+    //         }
+    //         width="30"
+    //         height="30"
+    //         alt={
+    //           isDark ?
+    //           "Light theme" :
+    //           "Dark theme"
+    //         }
+    //       />
+    //     </ToggleButton>
+    //   </ButtonGroup>
+    // </div>
+    <div>
+      <Switch
+        checked={isDark}
+        color="default"
+        onChange={() => onToggleTheme()}
+        className='theme-btn'
+        title={
+          isDark ?
+          "Light theme" :
+          "Dark theme"
+        }
+        sx={{
+        }}
+      />
     </div>
   );
 }
